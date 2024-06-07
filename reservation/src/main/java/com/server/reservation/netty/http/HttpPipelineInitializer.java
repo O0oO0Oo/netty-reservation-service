179fd30c2d6a0f3918bd45eb5c0f7a59c08ca05b
BA <windy01999@gmail.com>
2024-06-07 20:36:09 +0900

    feat: impl basic netty server

---
+++ reservation/src/main/java/com/server/reservation/netty/http/HttpPipelineInitializer.java
@@ -0,0 +1,38 @@
+package com.server.reservation.netty.http;
+
+import com.server.reservation.netty.http.handler.ExceptionHandler;
+import com.server.reservation.netty.http.handler.RouteMappingHandler;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class HttpPipelineInitializer extends ChannelInitializer<Channel> {
+    private boolean isClient = false;
+
+    /**
+     * @param isClient default true
+     */
+    public HttpPipelineInitializer(boolean isClient) {
+        this.isClient = isClient;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        if(isClient){
+            pipeline.addLast("httpClientCodec", new HttpClientCodec());
+        }else {
+            pipeline.addLast("httpServerCodec", new HttpServerCodec());
+        }
+
+        pipeline.addLast("httpObjectAggregator", new HttpObjectAggregator(512 * 1024));
+        pipeline.addLast("routeMappingHandler", new RouteMappingHandler());
+        pipeline.addLast("exceptionHandler", new ExceptionHandler());
+    }
+}
