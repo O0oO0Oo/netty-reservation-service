31fef3f04ca8889f254457aaf847592daf9160ac
BA <windy01999@gmail.com>
2024-08-21 00:51:36 +0900

    feat: add commonly used doamins

---
+++ reservation-saga/common/src/main/java/org/server/rsaga/common/domain/Money.java
@@ -0,0 +1,35 @@
+package org.server.rsaga.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Value;
+
+@Value
+@Embeddable
+public class Money {
+    @Column(nullable = false)
+    Long amount;
+
+    public Money(Long amount) {
+        if (amount == null || amount < 0) {
+            throw new IllegalArgumentException("The money amount must be a negative value.");
+        }
+        this.amount = amount;
+    }
+
+    public Money add(Money other) {
+        return new Money(this.amount + other.amount);
+    }
+
+    public Money subtract(Money other) {
+        long calculatedAmount = this.amount - other.amount;
+        if (calculatedAmount < 0) {
+            throw new IllegalArgumentException("The calculated money amount must not be a negative value.");
+        }
+        return new Money(calculatedAmount);
+    }
+
+    protected Money(){
+        this.amount = 0L;
+    }
+}
